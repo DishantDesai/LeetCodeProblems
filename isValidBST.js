f72c5dd45fdcd94c8247069b7356f794c8a6d4f5
Dishant <dishant0170@gmail.com>
2022-06-13 22:23:03 -0400

    check if tree is valid BST
    - first convert tree in to in-order DFS list
    - check if any bad node found iterating through in-order list

---
+++ isValidBST.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+//Solution 2
+var isValidBST = function (root) {
+  let order = [];
+  function traverse(node) {
+    if (node.left) traverse(node.left);
+    order.push(node.val);
+    if (node.right) traverse(node.right);
+  }
+  traverse(root);
+  for (let i = 1; i < order.length; i++) {
+    if (order[i] <= order[i - 1]) return false;
+  }
+  return true;
+};
